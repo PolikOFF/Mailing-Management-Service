085f089ff01dbc1ce5cb055e87ee17176a68aac9
Andrew_Polik <polik.24@mail.ru>
2024-03-02 00:14:46 +0300

    Описана модель получения отчета для клиента. Дополнены модели User, Client, Sending.

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class User(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Имя')
-    email = models.EmailField(max_length=200, verbose_name='Email')
+    name = models.CharField(max_length=100, verbose_name='Имя пользователя')
+    email = models.EmailField(max_length=100, verbose_name='Email')
 
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=100, verbose_name='Имя')
-    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
-    email = models.EmailField(max_length=200, verbose_name='Email')
+    first_name = models.CharField(max_length=100, verbose_name='Имя клиента')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия', **NULLABLE)
+    email = models.EmailField(max_length=100, verbose_name='Email')
 
 
-class Logs(models.Model):
-    pass
+class LogsForClient(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название рассылки')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
+    sent = models.BooleanField(default=False, verbose_name='статус отправки')
+    is_active = models.BooleanField(default=True, verbose_name='признак активности')
 
 
 class Sending(models.Model):
-    email = models.EmailField(max_length=200, verbose_name='Email')
+    email = models.EmailField(max_length=100, verbose_name='Email')
     name = models.CharField(max_length=100, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия', **NULLABLE)
+    message = models.TextField(verbose_name='Сообщение')
